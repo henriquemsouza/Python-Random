448602fd81b5ab02f986c0b704d38b56bf217cb5
Henrique Martins <henrique.m_s@hotmail.com>
2017-03-15 22:02:45 -0300

    Create SomaSimples.py

---
+++ SomaSimples.py
@@ -0,0 +1,12 @@
+def calc(a1,a2):
+    a4=int(a1)#Convert from string to int
+    a5=int(a2)
+    print(" Resultado\n",a4+a5)#Displays the result of the account
+
+
+
+b=input("valor 1:   ")
+c=input("valor 2:   ")
+
+
+calc(b,c)
