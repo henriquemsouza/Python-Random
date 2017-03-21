801289b3d301dbdb9e793f778f9458df849d5b76
Henrique Martins <henrique.m_s@hotmail.com>
2017-03-20 21:27:43 -0300

    working with the get user  and  join

--- basic/MakeFolder.py
+++ basic/MakeFolder.py
@@ -1,5 +1,8 @@
 import os
+import getpass
 
-path="./save"#name of  the folder created
+path=os.path.join(*["C:/Users/"+getpass.getuser()+"/Documents/save"])
+print(path)
 
 os.makedirs(path, exist_ok=True)
+
